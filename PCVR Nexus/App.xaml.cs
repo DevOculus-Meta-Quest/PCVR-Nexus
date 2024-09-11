20fb4b1a70300b4cd9852b22d0061a3a8ea6183d
Eliminater74 <eliminater74@gmail.com>
2024-09-11 19:32:14 -0400

    Add project files.

---
+++ PCVR Nexus/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PCVR_Nexus
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
